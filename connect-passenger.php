317de5f9d00dbba40ade3458f3812d5c289f0310
jate-koh <jate_ben@hotmail.com>
2022-04-23 16:58:42 +0700

    transfer data to db success

--- connect-passenger.php
+++ connect-passenger.php
@@ -1,25 +1,38 @@
 <?php
-    $PassportNo = $_POST['passportno'];
-    $FirstName = $_POST['firstname'];
-    $SurName = $_POST['lastname'];
-    $DOB = $_POST['date'];
-    $Sex = $_POST['sex'];
-    $Phone = $_POST['phone'];
-    $Address = $_POST['address'];
-    $Email = $_POST['email'];
-    $Nationality = $_POST['nationality'];
-    $PassportExp = $_POST['passportexp'];
+ //Database Connection
+ $conn = new mysqli('localhost','a9_cpe231_a9','jk17jk17','a9_Angel_Airline');
+ if($conn -> connect_error) {
+    die('connection failed. '.$conn -> connect_error);
+ }
+ else {
+    $PassportNo = mysqli_real_escape_string($conn,$_POST['passportno']);
+    $FirstName =  mysqli_real_escape_string($conn,$_POST['firstname']);
+    $SurName =  mysqli_real_escape_string($conn,$_POST['lastname']);
+    $DOB =  mysqli_real_escape_string($conn,$_POST['date']);
+    $Sex =  mysqli_real_escape_string($conn,$_POST['sex']);
+    $Phone =  mysqli_real_escape_string($conn,$_POST['phone']);
+    $Address =  mysqli_real_escape_string($conn,$_POST['address']);
+    $Email =  mysqli_real_escape_string($conn,$_POST['email']);
+    $Nationality =  mysqli_real_escape_string($conn,$_POST['nationality']);
+    $PassportExp =  mysqli_real_escape_string($conn,$_POST['passportexp']);
 
-    //Database Connection
-    $conn = new mysqli('localhost','a9_cpe231_a9','jk17jk17','a9_Angel_Airline');
-    if($conn -> connect_error) {
-        die('Connection Failed : '.$conn -> connect_error);
+    $sql= "INSERT INTO passenger(PassportNo,FirstName,SurName,DoB,Sex,Phone,Address,Email,Nationality,PassportExp)
+    VALUES('$PassportNo','$FirstName','$SurName','$DOB','$Sex','$Phone','$Address','$Email','$Nationality','$PassportExp')";
+    if(!mysqli_query($conn,$sql)) {
+       die('error. '.mysqli_error($conn));
     }
     else {
-        //$statement = $conn -> prepare("insert into passenger(passportno,firstname,lastname,date,sex,phone,address,email,nationality,passportexp)
-        //value(?,?,?,?,?,?,?,?,?,?)");
-        //$statement -> bind_param("ssssssssss",$PassportNo,$FirstName,$SurName,$DOB,$Sex,$Phone,$Address,$Email,$Nationality,$PassportExp);
-        //$statement -> execute();
         echo "success.";
     }
+}
+/*
+        $statement = $conn -> prepare(
+            "insert into passenger(passportno,firstname,lastname,date,sex,phone,address,email,nationality,passportexp)
+            value(?,?,?,?,?,?,?,?,?,?)"
+        );
+        $statement -> bind_param("ssssssssss",$PassportNo,$FirstName,$SurName,$DOB,$Sex,$Phone,$Address,$Email,$Nationality,$PassportExp);
+        $statement -> execute();
+        echo "success.";
+*/
 ?>
+
